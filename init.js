66768c8cea6dd10040de1c796ac521d185c8e615
Hoang <21hoangphan@gmail.com>
2022-04-01 01:03:59 -0700

    init

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const app = express();
 const path = require('path');
+const texpress = require('express');
+const app = express();
 
 /*use files in the public folder */
 app.use(express.static(path.join(__dirname, 'public')));
