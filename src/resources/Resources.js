02714a3bab29bc8bb427529b8afa11e54ba02847
2mnguyen1 <2mnguyen1@gmail.com>
2022-06-17 15:09:05 -0700

    adding resources

--- src/resources/Resources.js
+++ src/resources/Resources.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from "react";
+import "./resources.css";
 
-const Resources = () => {
-	return <div>Resources</div>;
-};
-
-export default Resources;
+export default function Resources() {
+  return (
+    <div className="resources-container">
+      <div className="resources-wrapper">
+        <div className="resources-info">
+          <div className="resources-title">Scholarships & Programs</div>
+          <div className="resources-desc">
+			
+		  </div>
+        </div>
+        <div className="resources-info">
+          <div className="resources-title">Must Reads</div>
+          <div className="resources-desc"></div>
+        </div>
+        <div className="resources-info">
+          <div className="resources-title">Similar Orgs</div>
+          <div className="resources-desc"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
