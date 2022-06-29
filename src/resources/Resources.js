cb58cfa1adbf8b3b0ca43d6dcf34012a461e58b6
= <minhngo2814@gmail.com>
2022-06-28 22:01:29 -0700

    add some resources css and setup js

--- src/resources/Resources.js
+++ src/resources/Resources.js
@@ -1,50 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./resources.css";
 
 export default function Resources() {
   return (
-    <div className="resources-container">
+
+    <>
+      <h1>Resources Page</h1>
+      <Tab />
+      <div className="resources-container">
       <div className="resources-wrapper">
         <div className="resources-info">
           <div className="resources-title">Scholarships & Programs</div>
-          <div className="resources-desc">
-            <span>Scholarships:</span>
-            <ul className="resources-list">
-              <li>Our scholarship application period is currently closed.</li>
-            </ul>
-            <span>Programs:</span>
-            <ul className="resources-list">
-              <li>Early Research Scholars Programs.</li>
-            </ul>
+            <div className="resources-desc">
+              <span>Scholarships:</span>
+              <ul className="resources-list">
+                <li>Our scholarship application period is currently closed.</li>
+              </ul>
+              <span>Programs:</span>
+              <ul className="resources-list">
+                <li>Early Research Scholars Programs.</li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div className="resources-info">
-          <div className="resources-title">Must Reads</div>
-          <div className="resources-desc">
-            <span>Scholarships:</span>
-            <ul className="resources-list">
-              <li>Our scholarship application period is currently closed.</li>
-            </ul>
-            <span>Programs:</span>
-            <ul className="resources-list">
-              <li>Early Research Scholars Programs.</li>
-            </ul>
+          <div className="resources-info">
+            <div className="resources-title">Must Reads</div>
+            <div className="resources-desc">
+              <span>Scholarships:</span>
+              <ul className="resources-list">
+                <li>Our scholarship application period is currently closed.</li>
+              </ul>
+              <span>Programs:</span>
+              <ul className="resources-list">
+                <li>Early Research Scholars Programs.</li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div className="resources-info">
-          <div className="resources-title">Similar Orgs</div>
-          <div className="resources-desc">
-            <span>Scholarships:</span>
-            <ul className="resources-list">
-              <li>Our scholarship application period is currently closed.</li>
-            </ul>
-            <span>Programs:</span>
-            <ul className="resources-list">
-              <li>Early Research Scholars Programs.</li>
-            </ul>
+          <div className="resources-info">
+            <div className="resources-title">Similar Orgs</div>
+            <div className="resources-desc">
+              <span>Scholarships:</span>
+              <ul className="resources-list">
+                <li>Our scholarship application period is currently closed.</li>
+              </ul>
+              <span>Programs:</span>
+              <ul className="resources-list">
+                <li>Early Research Scholars Programs.</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
+
+ 
   );
+}
+
+function Tab() {
+
+  const [tabState, setTab] = useState(1);
+
+  const toggleTab = (index) => {
+    setTab(index)
+  }
+
+  return (
+    <div className="tab-container">
+      <h1>This the resource tab component</h1>
+      <button>Tutorial</button>
+      <button>Readings</button>
+      <button>Orgs</button>
+      <div className="tab-dropdown container">
+        <div className="tab-dropdown-content">
+          <h3>Tutorial Content</h3>
+        </div>
+        <div className="tab-dropdown-content">
+          <h3>Tutorial Content</h3>
+        </div>
+        <div className="tab-dropdown-content">
+          <h3>Tutorial Content</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+function TabContent () {
+  
+
+  return 
+  (
+    <>
+    </>
+  )
 }
