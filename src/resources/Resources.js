e16d2457b9cd84f46f0c40f496930678f8970ac5
Kevin Bui <khvbui@gmail.com>
2022-07-04 18:42:15 -0700

    Implement React Tabs for Resources Part of Website
    -Copied over React Tab code from Activity.js to implement tabs
    -Copied over css from activity.css to resources.css
    -Created skeleton for Tutorials.js, Research.js, Organizations.js
    -Edited NavigationBar.js so dropdown-links redirect Resources links properly
    -Edited NavigationBar.js to rename "Event" to "Events", "Project" to "Projects", and "Tutorial" to "Tutorials"

--- src/resources/Resources.js
+++ src/resources/Resources.js
@@ -1,50 +1,82 @@
-import React from "react";
+import React, { useState, useRef } from 'react';
 import "./resources.css";
+import Tutorials from './Tutorials.js';
+import Research from './Research';
+import Organizations from './Organizations';
 
-export default function Resources() {
+/*
+Create card tab Tutorials | Research | Organizations both underline + hover highlight box
+v-Link tab to component -> how to link subpage
+Use Hook to change card tab
+Create Tutorials, Research, and Organizations Container
+Container:
+
+Square box: Dat center large font 46px, Time below
+Click view detail, box will expand
+
+Line break
+
+Container includes:
+	Shadow: Hover expand
+	Card: Title, Url, Description
+*/
+
+const Resources = () => {
   return (
     <div className="resources-container">
-      <div className="resources-wrapper">
-        <div className="resources-info">
-          <div className="resources-title">Scholarships & Programs</div>
-          <div className="resources-desc">
-            <span>Scholarships:</span>
-            <ul className="resources-list">
-              <li>Our scholarship application period is currently closed.</li>
-            </ul>
-            <span>Programs:</span>
-            <ul className="resources-list">
-              <li>Early Research Scholars Programs.</li>
-            </ul>
-          </div>
+      <h1 className="resource-title">Resources</h1>
+      <Tab className="tab-components"/>
+    </div>
+  );
+};
+
+export default Resources;
+
+function Tab() {
+  const [tabState, setTab] = useState(1)
+
+  const toggleTab = (index) => {
+    setTab(index)
+  }
+
+  return (
+    <div className='tab-container'>
+      {/*
+        <Link to='/resources/tutorials' className='tab-button'>Tutorials</Link>
+        <Link to='resources/research' className='tab-button'>Research</Link>
+        <Link to='resources/organizations' className='tab-button'>Organizations</Link>
+      */}
+      <div className='tabs'>
+        <button type='button' onClick={() => toggleTab(1)} className={tabState === 1 ? 'tab-button active' : 'tab-button'}>Tutorials</button>
+        <button type='button' onClick={() => toggleTab(2)} className={tabState === 2 ? 'tab-button active' : 'tab-button'}>Research</button>
+        <button type='button' onClick={() => toggleTab(3)} className={tabState === 3 ? 'tab-button active' : 'tab-button'}>Organizations</button>
+      </div>
+
+      <div className="tab-contents">
+        <div className={tabState === 1 ? 'tab-content active-content': 'tab-content'}>
+          <h2>Tutorials</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+							sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+							nisi ut aliquip ex ea commodo consequat.
+          </p>
         </div>
-        <div className="resources-info">
-          <div className="resources-title">Must Reads</div>
-          <div className="resources-desc">
-            <span>Scholarships:</span>
-            <ul className="resources-list">
-              <li>Our scholarship application period is currently closed.</li>
-            </ul>
-            <span>Programs:</span>
-            <ul className="resources-list">
-              <li>Early Research Scholars Programs.</li>
-            </ul>
-          </div>
+
+        <div  className={tabState === 2 ? 'tab-content active-content': 'tab-content'}>
+          <h2>Research</h2>
+          <p>Something about Early Research Scholars Programs.
+          </p>
         </div>
-        <div className="resources-info">
-          <div className="resources-title">Similar Orgs</div>
-          <div className="resources-desc">
-            <span>Scholarships:</span>
-            <ul className="resources-list">
-              <li>Our scholarship application period is currently closed.</li>
-            </ul>
-            <span>Programs:</span>
-            <ul className="resources-list">
-              <li>Early Research Scholars Programs.</li>
-            </ul>
-          </div>
+
+        <div  className={tabState === 3 ? 'tab-content active-content': 'tab-content'}>
+          <h2>Organizations</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+							sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+							nisi ut aliquip ex ea commodo consequat.
+          </p>
         </div>
       </div>
     </div>
-  );
+  )
 }
