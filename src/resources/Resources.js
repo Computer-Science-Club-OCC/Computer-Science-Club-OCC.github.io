b674e2f5659ba25008e1878337b12e1f6d2efcb2
minhngo <minhngo2814@gmail.com>
2022-07-27 21:23:44 -0700

    mapping and render fake data

--- src/resources/Resources.js
+++ src/resources/Resources.js
@@ -1,50 +1,133 @@
-import React from "react";
+import { useState } from "react";
+import { tutorialsMap, readingsMap, organizationsMap } from "./fakeData";
 import "./resources.css";
 
-export default function Resources() {
-  return (
-    <div className="resources-container">
-      <div className="resources-wrapper">
-        <div className="resources-info">
-          <div className="resources-title">Scholarships & Programs</div>
-          <div className="resources-desc">
-            <span>Scholarships:</span>
-            <ul className="resources-list">
-              <li>Our scholarship application period is currently closed.</li>
-            </ul>
-            <span>Programs:</span>
-            <ul className="resources-list">
-              <li>Early Research Scholars Programs.</li>
-            </ul>
-          </div>
-        </div>
-        <div className="resources-info">
-          <div className="resources-title">Must Reads</div>
-          <div className="resources-desc">
-            <span>Scholarships:</span>
-            <ul className="resources-list">
-              <li>Our scholarship application period is currently closed.</li>
-            </ul>
-            <span>Programs:</span>
-            <ul className="resources-list">
-              <li>Early Research Scholars Programs.</li>
-            </ul>
-          </div>
-        </div>
-        <div className="resources-info">
-          <div className="resources-title">Similar Orgs</div>
-          <div className="resources-desc">
-            <span>Scholarships:</span>
-            <ul className="resources-list">
-              <li>Our scholarship application period is currently closed.</li>
-            </ul>
-            <span>Programs:</span>
-            <ul className="resources-list">
-              <li>Early Research Scholars Programs.</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+function Resources() {
+	return (
+		<div className="resources-container">
+			<Tab />
+		</div>
+	);
+}
+export default Resources;
+
+// List of buttons
+const buttonsList = ["Tutorials", "Readings", "Organization"];
+
+// Render button
+function renderButtons(toggleTab, tabState) {
+	return buttonsList.map(tabButton => {
+		return (
+			<button
+				key={buttonsList.indexOf(tabButton) + 1}
+				type="button"
+				onClick={() => toggleTab(buttonsList.indexOf(tabButton) + 1)}
+				className={
+					tabState === buttonsList.indexOf(tabButton) + 1 ? "active" : ""
+				}
+			>
+				{tabButton}
+			</button>
+		);
+	});
+}
+
+function renderTutorialContents(tabState) {
+	const contents = tutorialsMap.map(tutorial => {
+		return (
+			<li key={tutorial.id}>
+				<h3>{tutorial.title}</h3>
+				<p>{tutorial.description}</p>
+				<video controls muted>
+					<source src={tutorial.url} type="video/mp4" alt="text" />
+				</video>
+				<ul>
+					<li>{tutorial.tags}</li>
+				</ul>
+			</li>
+		);
+	});
+	return (
+		<div
+			className={
+				tabState === 1 ? "resources-content active" : "resources-content"
+			}
+		>
+			{contents}
+		</div>
+	);
+}
+
+function renderReadingsContent(tabState) {
+	const contents = readingsMap.map(reading => {
+		return (
+			<li key={reading.id}>
+				<h3>Title: {reading.title}</h3>
+				<button type="button">{reading.publicationDate}</button>
+				<p>Description: {reading.description}</p>
+				<p>
+					<a href={reading.url}>Get Content</a>
+				</p>
+				<ul>
+					<li>{reading.tags}</li>
+				</ul>
+			</li>
+		);
+	});
+
+	return (
+		<div
+			className={
+				tabState === 2 ? "resources-content active" : "resources-content"
+			}
+		>
+			{contents}
+		</div>
+	);
+}
+
+function renderOrganizationsContent(tabState) {
+	const contents = organizationsMap.map(org => {
+		return (
+			<li key={org.id}>
+				<h3>{org.title}</h3>
+				<p>{org.description}</p>
+				<a type="button" href={org.url}>
+					Get Link
+				</a>
+				<ul>
+					<li>{org.tags}</li>
+				</ul>
+			</li>
+		);
+	});
+
+	return (
+		<div
+			className={
+				tabState === 3 ? "resources-content active" : "resources-content"
+			}
+		>
+			{contents}
+		</div>
+	);
+}
+
+function Tab() {
+	const [tabState, setTab] = useState(1);
+
+	const toggleTab = index => {
+		setTab(index);
+	};
+
+	return (
+		<>
+			<div className="resources-tabs">{renderButtons(toggleTab, tabState)}</div>
+			<div className="resources-contents">
+				{renderTutorialContents(tabState)}
+				{renderReadingsContent(tabState)}
+				{renderOrganizationsContent(tabState)}
+			</div>
+		</>
+	);
 }
