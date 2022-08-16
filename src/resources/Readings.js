0ef6d026350d06385f395ae618bbc490fdbbc361
Kevin Bui <khvbui@gmail.com>
2022-08-15 21:24:58 -0700

    Render fakeData for Readings

--- src/resources/Readings.js
+++ src/resources/Readings.js
@@ -1,16 +1,73 @@
-import React from "react";
+import PropTypes from "prop-types";
+import { useState } from "react";
 import "./resources.css";
+
+const fakeData = require("./fakeData");
+
+// tags is currently unused
+function ToggleItem({ id, title, publicationDate, description, url, tags }) {
+	const [showButton, setShowButton] = useState(false);
+
+	return (
+		<li className="resource-list-item" key={id}>
+			<button
+				type="button"
+				className={`resource-list-title ${
+					showButton && "resource-list-title-active"
+				}`}
+				onClick={() => setShowButton(prevState => !prevState)}
+			>
+				{title}
+			</button>
+
+			<li
+				className={`resource-list-description ${
+					showButton && "resource-list-description-active"
+				}`}
+			>
+				{description}
+				{"\n\n"}
+				<a
+					className="resource-link"
+					href={url}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Read it here. {/* Open link in new tab*/}
+				</a>
+				{"\n\n"}
+				Publication date: {publicationDate}
+				{"\n"}
+				Tags: {tags.join(", ")}
+			</li>
+		</li>
+	);
+}
+
+ToggleItem.propTypes = {
+	id: PropTypes.number.isRequired,
+	title: PropTypes.string.isRequired,
+	publicationDate: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	url: PropTypes.string.isRequired,
+	tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 function Readings() {
 	return (
 		<div>
-			<h2>Readings</h2>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-				commodo consequat.
-			</p>
+			{fakeData.readingsMap.map(reading => {
+				return (
+					<ToggleItem
+						id={reading.id}
+						title={reading.title}
+						publicationDate={reading.publicationDate}
+						description={reading.description}
+						url={reading.url}
+						tags={reading.tags}
+					/>
+				);
+			})}
 		</div>
 	);
 }
