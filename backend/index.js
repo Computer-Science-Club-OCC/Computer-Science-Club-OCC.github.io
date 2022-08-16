3c4e74fb8c266472b94b329489b129a66cacb3e7
minhngo <minhngo2814@gmail.com>
2022-08-15 21:40:07 -0700

    setup axios & eslint-prettier for backend

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require("express")
+const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
-const app = express();
-const PORT = process.env.PORT || 8000;
-require('dotenv/config');
+const app = express()
+const PORT = process.env.PORT || 8000
+require("dotenv/config")
 
 // Add your code here
 // Import Routes
-const eventsRoute = require("./routes/events");
-const projectsRoute = require("./routes/projects");
-const tutorialsRoute = require("./routes/tutorials");
-const readingsRoute = require("./routes/readings");
-const organizationsRoute = require("./routes/organizations"); 
+const eventsRoute = require("./routes/events")
+const projectsRoute = require("./routes/projects")
+const tutorialsRoute = require("./routes/tutorials")
+const readingsRoute = require("./routes/readings")
+const organizationsRoute = require("./routes/organizations")
 
 // Use Routes
 
-
-
 // Test backend run
-app.get('/', (request, respond) => {
-  console.log(`${request.protocol}://${request.get('host')}${request.originalUrl}`)
-  respond.send("<h1>Backend is running!</h1>")
+app.get("/", (request, respond) => {
+    console.log(
+        `${request.protocol}://${request.get("host")}${request.originalUrl}`
+    )
+    respond.send("<h1>Backend is running!</h1>")
 })
 
 app.listen(PORT)
--- backend/models/Organizations.js
+++ backend/models/Organizations.js
@@ -1,3 +1,3 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 // Add your code here
