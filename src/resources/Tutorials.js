4a9412db52832a9237d329ae4e467ea7dfa88e4d
Kevin Bui <khvbui@gmail.com>
2022-08-15 21:24:51 -0700

    Render fakeData for Tutorials

--- src/resources/Tutorials.js
+++ src/resources/Tutorials.js
@@ -1,17 +1,70 @@
-import React from "react";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import Youtube from "react-youtube";
 import "./resources.css";
+
+const fakeData = require("./fakeData");
+
+// tags is currently unused
+function ToggleItem({ id, title, description, url, tags }) {
+	const [showButton, setShowButton] = useState(false);
+
+	return (
+		<li className="resource-list-item" key={id}>
+			<button
+				type="button"
+				className={`resource-list-title ${
+					showButton && "resource-list-title-active"
+				}`}
+				onClick={() => setShowButton(prevState => !prevState)}
+			>
+				{title}
+			</button>
+
+			<li
+				className={`resource-list-description ${
+					showButton && "resource-list-description-active"
+				}`}
+			>
+				{description}
+				{"\n\n"}
+				<a
+					className="resource-link"
+					href={url}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Watch the video here. {/* Will open link in new tab*/}
+				</a>
+				{"\n\n"}
+				Tags: {tags.join(", ")}
+			</li>
+		</li>
+	);
+}
+
+ToggleItem.propTypes = {
+	id: PropTypes.number.isRequired,
+	title: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	url: PropTypes.string.isRequired,
+	tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 function Tutorials() {
 	return (
-		<div>
-			<h2>Tutorials</h2>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-				commodo consequat.
-			</p>
-		</div>
+		<ul className="resource-list">
+			{fakeData.tutorialsMap.map(tutorial => (
+				<ToggleItem
+					key={tutorial.id}
+					id={tutorial.id}
+					title={tutorial.title}
+					description={tutorial.description}
+					url={tutorial.url}
+					tags={tutorial.tags}
+				/>
+			))}
+		</ul>
 	);
 }
 
