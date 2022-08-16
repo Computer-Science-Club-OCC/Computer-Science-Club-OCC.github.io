677be794f43c410fada90b09b52b207c0148b474
minhngo <minhngo2814@gmail.com>
2022-08-15 21:10:07 -0700

    minor updates

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser")
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8000;
 require('dotenv/config');
 
 // Add your code here
+// Import Routes
+const eventsRoute = require("./routes/events");
+const projectsRoute = require("./routes/projects");
+const tutorialsRoute = require("./routes/tutorials");
+const readingsRoute = require("./routes/readings");
+const organizationsRoute = require("./routes/organizations"); 
+
+// Use Routes
+
+
+
+// Test backend run
+app.get('/', (request, respond) => {
+  console.log(`${request.protocol}://${request.get('host')}${request.originalUrl}`)
+  respond.send("<h1>Backend is running!</h1>")
+})
+
+app.listen(PORT)
