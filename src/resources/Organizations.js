7c01ba4c4c94802019ea5fe7544e31d7b2c8bb70
Kevin Bui <khvbui@gmail.com>
2022-08-15 21:25:04 -0700

    Render fakeData for Organizations

--- src/resources/Organizations.js
+++ src/resources/Organizations.js
@@ -1,17 +1,69 @@
-import React from "react";
+import PropTypes from "prop-types";
+import { useState } from "react";
 import "./resources.css";
+
+const fakeData = require("./fakeData");
+
+// tags is currently unused
+function ToggleItem({ id, title, description, url, tags }) {
+	const [showButton, setShowButton] = useState(false);
+
+	return (
+		<li className="resource-list-item" key={id}>
+			<button
+				type="button"
+				className={`resource-list-title ${
+					showButton && "resource-list-title-active"
+				}`}
+				onClick={() => setShowButton(prevState => !prevState)}
+			>
+				{title}
+			</button>
+
+			<li
+				className={`resource-list-description ${
+					showButton && "resource-list-description-active"
+				}`}
+			>
+				{description}
+				{"\n\n"}
+				<a
+					className="resource-link"
+					href={url}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn more here. {/* Open link in new tab*/}
+				</a>
+				{"\n\n"}
+				Tags: {tags.join(", ")}
+			</li>
+		</li>
+	);
+}
+
+ToggleItem.propTypes = {
+	id: PropTypes.number.isRequired,
+	title: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	url: PropTypes.string.isRequired,
+	tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 function Organizations() {
 	return (
-		<div>
-			<h2>Organizations</h2>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-				commodo consequat.
-			</p>
-		</div>
+		<ul className="resource-list">
+			{fakeData.organizationsMap.map(org => (
+				<ToggleItem
+					key={org.id}
+					id={org.id}
+					title={org.title}
+					description={org.description}
+					url={org.url}
+					tags={org.tags}
+				/>
+			))}
+		</ul>
 	);
 }
 
