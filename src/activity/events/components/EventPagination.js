2fdb9c8bd42e95e86e621989cf31e80b654c234a
minhngo <minhngo2814@gmail.com>
2022-08-18 21:32:51 -0700

    relatively shrink image, pagination

--- src/activity/events/components/EventPagination.js
+++ src/activity/events/components/EventPagination.js
@@ -1,16 +1,39 @@
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Box, Pagination } from "@mui/material";
 
 function EventPagination({ onPageChanged, page, pageSize, total }) {
+	const [paginationSize, setPaginationSize] = useState("large");
+	const [screenSize, getDimension] = useState({
+		dynamicWidth: window.innerWidth,
+	});
+
+	const setDimension = () => {
+		getDimension({
+			dynamicWidth: window.innerWidth,
+		});
+	};
+
+	useEffect(() => {
+		window.addEventListener("resize", setDimension);
+		if (screenSize.dynamicWidth >= 960) {
+			setPaginationSize("large");
+		}
+		if (screenSize.dynamicWidth < 960) {
+			setPaginationSize("medium");
+		}
+		if (screenSize.dynamicWidth < 640) {
+			setPaginationSize("small");
+		}
+		return () => {
+			window.removeEventListener("resize", setDimension);
+		};
+	}, [screenSize]);
+
 	return (
-		<Box
-			justifyContent="center"
-			alignItems="center"
-			display="flex"
-			sx={{ margin: "10px 0px" }}
-		>
+		<Box className="pagination-box">
 			<Pagination
-				size="large"
+				size={paginationSize}
 				count={Math.ceil(total / pageSize)}
 				page={page}
 				onChange={onPageChanged}
