31e8b072cc349f2acf5232424f26af3f39179c75
jameslindfors <jameslindfors@outlook.com>
2022-09-23 22:17:15 -0700

    create orgs model

--- backend/models/organizations/orgs-model.js
+++ backend/models/organizations/orgs-model.js
@@ -1,4 +1,19 @@
-// const mongoose = require("mongoose")
-// // const { Schema } = mongoose
+const mongoose = require("mongoose")
+const { Schema } = mongoose
 
-// Add your code here
+const orgSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    url: String,
+    tags: [String],
+    created_at: Date,
+    updated_at: Date,
+})
+
+module.exports = mongoose.model("organizations", orgSchema)
