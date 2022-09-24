03e98ce1dd21a097df84ac65993fe090b285a9c5
jameslindfors <jameslindfors@outlook.com>
2022-09-23 21:27:39 -0700

    projects model tests

--- backend/models/projects/projects-model.test.js
+++ backend/models/projects/projects-model.test.js
@@ -1,8 +1,41 @@
 // const mongoose = require("mongoose")
-// const projectsModel = require("./projects-model")
+const ProjectsModel = require("./projects-model")
 const db = require("../db-test-setup")
 
 // Define test instances here
+const testProject1 = {
+    title: "Project 1",
+    status: "In Progress",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+        "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco " +
+        "laboris nisi ut aliquip ex ea commodo consequat. ",
+    start_date: new Date("12-24-2022"),
+    end_date: new Date("12-24-2023"),
+    discord_url: "https://discord.gg/12345",
+    github_url: "github.com/12345",
+    demo_url: "https://demo.com/12345",
+    created_at: new Date("09-27-2022"),
+    updated_at: new Date("09-30-2022"),
+}
+
+const testProject2 = {
+    title: "Project 2",
+    status: "In Progress",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+        "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco " +
+        "laboris nisi ut aliquip ex ea commodo consequat. ",
+    start_date: new Date("12-24-2022"),
+    end_date: new Date("12-24-2023"),
+    discord_url: "https://discord.gg/12345",
+    github_url: "github.com/12345",
+    demo_url: "https://demo.com/12345",
+    created_at: new Date("09-27-2022"),
+    updated_at: new Date("09-30-2022"),
+}
 
 // Database for testing
 beforeAll(async () => await db.connect())
@@ -11,7 +44,133 @@ afterAll(async () => await db.closeDatabase())
 
 // Add your test here
 describe("Projects Model Tests", () => {
-    test("Dummy test", () => {
-        expect(true).toBe(true)
+    test("Database Has Projects Model", () => {
+        expect(ProjectsModel).toBeDefined()
+    })
+
+    describe("Project Test Series", () => {
+        test("Get Project Instance from Database", async () => {
+            const project = new ProjectsModel(testProject1)
+            await project.save()
+            const retrievedProject = await ProjectsModel.findOne({
+                title: "Project 1",
+            })
+
+            // Expected value set
+            const expectedTitle = testProject1.title
+            const expectedStatus = testProject1.status
+            const expectedDescription = testProject1.description
+            const expectedStartDate = testProject1.start_date
+            const expectedEndDate = testProject1.end_date
+            const expectedDiscordUrl = testProject1.discord_url
+            const expectedGithubUrl = testProject1.github_url
+            const expectedDemoUrl = testProject1.demo_url
+            const expectedCreatedAt = testProject1.created_at
+            const expectedUpdatedAt = testProject1.updated_at
+
+            // Tests
+            expect(expectedTitle).toEqual(retrievedProject.title)
+            expect(expectedStatus).toEqual(retrievedProject.status)
+            expect(expectedDescription).toEqual(retrievedProject.description)
+            expect(expectedStartDate).toEqual(retrievedProject.start_date)
+            expect(expectedEndDate).toEqual(retrievedProject.end_date)
+            expect(expectedDiscordUrl).toEqual(retrievedProject.discord_url)
+            expect(expectedGithubUrl).toEqual(retrievedProject.github_url)
+            expect(expectedDemoUrl).toEqual(retrievedProject.demo_url)
+            expect(expectedCreatedAt).toEqual(retrievedProject.created_at)
+            expect(expectedUpdatedAt).toEqual(retrievedProject.updated_at)
+        })
+
+        test("Add a Project to Database", async () => {
+            const project = new ProjectsModel(testProject1)
+
+            const createdProject = await project.save()
+
+            // Expected value set
+            const expectedTitle = testProject1.title
+            const expectedStatus = testProject1.status
+            const expectedDescription = testProject1.description
+            const expectedStartDate = testProject1.start_date
+            const expectedEndDate = testProject1.end_date
+            const expectedDiscordUrl = testProject1.discord_url
+            const expectedGithubUrl = testProject1.github_url
+            const expectedDemoUrl = testProject1.demo_url
+            const expectedCreatedAt = testProject1.created_at
+            const expectedUpdatedAt = testProject1.updated_at
+
+            // Tests
+            expect(createdProject.title).toEqual(expectedTitle)
+            expect(createdProject.status).toEqual(expectedStatus)
+            expect(createdProject.description).toEqual(expectedDescription)
+            expect(createdProject.start_date).toEqual(expectedStartDate)
+            expect(createdProject.end_date).toEqual(expectedEndDate)
+            expect(createdProject.discord_url).toEqual(expectedDiscordUrl)
+            expect(createdProject.github_url).toEqual(expectedGithubUrl)
+            expect(createdProject.demo_url).toEqual(expectedDemoUrl)
+            expect(createdProject.created_at).toEqual(expectedCreatedAt)
+            expect(createdProject.updated_at).toEqual(expectedUpdatedAt)
+        })
+
+        test("Update a Project in Database", async () => {
+            const project = new ProjectsModel(testProject1)
+            await project.save()
+            const retrievedProject = await ProjectsModel.findOne({
+                title: "Project 1",
+            })
+
+            // Update values
+            retrievedProject.title = "Project 1 Updated"
+            retrievedProject.status = "Completed"
+            retrievedProject.description = "Updated Description"
+            retrievedProject.start_date = new Date("12-24-2022")
+            retrievedProject.end_date = new Date("12-24-2023")
+            retrievedProject.discord_url = "https://discord.gg/12345"
+            retrievedProject.github_url = "github.com/12345"
+            retrievedProject.demo_url = "https://demo.com/12345"
+            retrievedProject.created_at = new Date("09-27-2022")
+            retrievedProject.updated_at = new Date("09-30-2022")
+
+            const updatedProject = await retrievedProject.save()
+
+            // Expected value set
+            const expectedTitle = retrievedProject.title
+            const expectedStatus = retrievedProject.status
+            const expectedDescription = retrievedProject.description
+            const expectedStartDate = retrievedProject.start_date
+            const expectedEndDate = retrievedProject.end_date
+            const expectedDiscordUrl = retrievedProject.discord_url
+            const expectedGithubUrl = retrievedProject.github_url
+            const expectedDemoUrl = retrievedProject.demo_url
+            const expectedCreatedAt = retrievedProject.created_at
+            const expectedUpdatedAt = retrievedProject.updated_at
+
+            // Tests
+            expect(updatedProject.title).toEqual(expectedTitle)
+            expect(updatedProject.status).toEqual(expectedStatus)
+            expect(updatedProject.description).toEqual(expectedDescription)
+            expect(updatedProject.start_date).toEqual(expectedStartDate)
+            expect(updatedProject.end_date).toEqual(expectedEndDate)
+            expect(updatedProject.discord_url).toEqual(expectedDiscordUrl)
+            expect(updatedProject.github_url).toEqual(expectedGithubUrl)
+            expect(updatedProject.demo_url).toEqual(expectedDemoUrl)
+            expect(updatedProject.created_at).toEqual(expectedCreatedAt)
+            expect(updatedProject.updated_at).toEqual(expectedUpdatedAt)
+        })
+
+        test("Delete a Project from Database", async () => {
+            const project = new ProjectsModel(testProject1)
+            await project.save()
+            const retrievedProject = await ProjectsModel.findOne({
+                title: "Project 1",
+            })
+
+            await retrievedProject.remove()
+
+            const deletedProject = await ProjectsModel.findOne({
+                title: "Project 1",
+            })
+
+            expect(deletedProject).toBeNull()
+        })
     })
 })
