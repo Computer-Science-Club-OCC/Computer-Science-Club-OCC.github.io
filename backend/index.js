c4084b243a9c9060211524bc2d58b8b779332a97
jameslindfors <jameslindfors@outlook.com>
2022-09-27 00:59:11 -0700

    register tags model

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,7 @@ mongoose
 // Will remove model imports once importing routes
 require("../backend/models/image/image-model")
 require("../backend/models/events/events-model")
+require("../backend/models/tags/tags-model")
 
 // Parsing
 app.use(express.urlencoded({ extended: true }))
