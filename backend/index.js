8416bf09a27d910dec4990deb2374d4267860c1d
= <minhngo2814@gmail.com>
2022-10-02 00:31:21 -0700

    static file uploads & dir name

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const path = require("path")
+const upload = require("./middlewares/upload-static")
 const checkMongoStatus = require("./utils/check-mongo-status")
 const PORT = process.env.PORT || 8000
 require("dotenv/config")
+
+// Base directory
+global.__basedir = __dirname
 
 // Connect datbase and create collection
 mongoose
@@ -20,15 +25,16 @@ mongoose
 // Import models
 // Notes: remove model imports won't add schemas to cs_club database on Linux
 // Will remove model imports once importing routes
-require("../backend/models/image/image-model")
 require("../backend/models/events/events-model")
 require("../backend/models/projects/projects-model")
 
 // Parsing
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(express.static(path.join(__basedir + "public")))
 
 // Import Routes
+const imagesRoute = require("./routes/images/images-route")
 // const eventsRoute = require("./routes/events/events-route")
 // const projectsRoute = require("./routes/projects/projects-route")
 // const tutorialsRoute = require("./routes/tutorials/tutorials-route")
@@ -36,6 +42,7 @@ app.use(express.json())
 // const organizationsRoute = require("./routes/organizations/orgs-route")
 
 // Use Routes
+app.use("/images", imagesRoute)
 
 // Run server
 app.listen(PORT, () => {
--- backend/routes/images/images-route.js
+++ backend/routes/images/images-route.js
@@ -2,9 +2,9 @@ const express = require("express")
 const router = express.Router()
 const path = require("path")
 
-// images services
+// image services
 router.get("/:pk", (req, res) => {
-    res.sendFile(path.join(__basedir, `/public/uploads/${res.params.pk}`))
+    res.sendFile(path.join(__basedir, `/public/uploads/${req.params.pk}`))
 })
 
 module.exports = router
