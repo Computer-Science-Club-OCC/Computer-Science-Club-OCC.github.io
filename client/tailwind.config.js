f499b117a0d435ce56bdf7e0ed1bed121542a625
jameslindfors <jameslindfors@outlook.com>
2022-10-01 21:39:08 -0700

    setup tailwindcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		colors: {
+			occ_color: "#b26910",
+		},
+		extend: {},
+	},
+	plugins: [],
+};
