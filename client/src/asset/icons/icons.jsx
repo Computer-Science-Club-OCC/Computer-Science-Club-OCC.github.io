dd491e964c827bd7f50419528a9a081339b042cc
minhngo <minhngo2814@gmail.com>
2022-10-26 20:37:15 -0700

    minhngo-change-nav-activity-icon

---
+++ client/src/asset/icons/icons.jsx
@@ -0,0 +1,33 @@
+const CloseX = (
+	<svg
+		fill="none"
+		stroke="currentColor"
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth={2}
+			d="M6 18L18 6M6 6l12 12"
+		/>
+	</svg>
+);
+
+const Menu = (
+	<svg
+		fill="none"
+		stroke="currentColor"
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth={2}
+			d="M4 6h16M4 12h16M4 18h16"
+		/>
+	</svg>
+);
+
+export { CloseX, Menu };
