0e24098268f060c63a5a00d1c07fa9d578a9bf3f
minhngo3818 <minhngo2814@gmail.com>
2022-11-01 16:41:05 -0700

    edit text

--- backend/middleware/passport-magic-login.js
+++ backend/middleware/passport-magic-login.js
@@ -2,15 +2,14 @@ const MagicLoginStrategy = require("passport-magic-login")
 
 const magicLogin = new MagicLoginStrategy({
     secret: process.env.MAGIC_LINK_SECRET,
-    callbackUrl: "/auth/magiclogin/callback",
+    callbackUrl: "/auth/login/callback",
     sendMagicLink: async (destination, href) => {
         await sendEmail({
             to: destination,
-            body: `Click this link to finish logging in: https://yourcompany.com${href}`,
+            body: `Click this link to finish logging in: ${href}`,
         })
     },
     verify: (payload, callback) => {
-        // Get or create a user with the provided email from the database
         getOrCreateUserWithEmail(payload.destination)
             .then((user) => {
                 callback(null, user)
