b3871b7f264a66ca2c4227d1e33f3f598c347b21
Siddhant <codebysidd@gmail.com>
2023-04-20 17:47:45 +0530

    All issues resolved

--- client/src/components/projects/project-card/Project-Card.jsx
+++ client/src/components/projects/project-card/Project-Card.jsx
@@ -5,6 +5,7 @@ import {
 	ProjectCardImg,
 	ProjectIcon,
 } from "../../../asset/icons/icons";
+import "./project-card.css"
 
 function ProjectCard({
 	title,
@@ -23,35 +24,34 @@ function ProjectCard({
 	};
 
 	return (
-		<div className="w-96 h-96 rounded-3xl bg-projectCardBg p-8 text-white drop-shadow-lg flex flex-col gap-4 hover:scale-110 mt-8 transition-all ease-in">
-			<div className="text-projectCardLinkHover flex justify-center items-center">
+		<div className="w-96 h-96 rounded-3xl bg-gray-med p-8 text-white drop-shadow-lg flex flex-col gap-4 hover:scale-110 mt-8 transition-all ease-in">
+			<div className="text-orange flex justify-center items-center">
 				<ProjectCardImg />
 			</div>
 			<div className="flex flex-col gap-6">
 				<p className="text-2xl">{title}</p>
-				<p className=" text-projectCardDescription text-base line-clamp-4">
+				<p className=" text-white text-base line-clamp-4">
 					{description}
 				</p>
 
-				<div className="flex gap-4 items-center relative">
+				<div className="flex gap-4 justify-between items-center relative">
 					<p className="text-base">
 						Status: <span className={statusColor(status)}>{status}</span>
 					</p>
-					<div className="projectCardLinks" data-tooltip="Github">
-						<a href={gitUrl}>
+                    <div className="flex flex-row flex-wrap gap-2">
+						<a className="project-card-link" data-tooltip="GitHub" href={gitUrl}>
 							<GithubIcon />
 						</a>
-					</div>
-					<div className="projectCardLinks" data-tooltip="Discord">
-						<a href={discordUrl}>
+						<a className="project-card-link" data-tooltip="Discord" href={discordUrl}>
 							<DiscordIcon />
 						</a>
-					</div>
-					<div className="projectCardLinks" data-tooltip="Project">
-						<a href={projectUrl}>
+						<a
+                        className="project-card-link" 
+                        data-tooltip="Project"
+                        href={projectUrl}>
 							<ProjectIcon />
 						</a>
-					</div>
+                    </div>
 				</div>
 			</div>
 		</div>
@@ -59,8 +59,3 @@ function ProjectCard({
 }
 
 export default ProjectCard;
-
-
-
-//file moved to correct folder
-//drop shadow fixed
