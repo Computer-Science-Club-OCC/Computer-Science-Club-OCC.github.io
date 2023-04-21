8f200aa93c284075767c316dd1eac5c28f367c4b
Siddhant <codebysidd@gmail.com>
2023-04-21 10:05:02 +0530

    Errors fixed

--- client/src/components/projects/project-card/Project-Card.jsx
+++ client/src/components/projects/project-card/Project-Card.jsx
@@ -5,7 +5,7 @@ import {
 	ProjectCardImg,
 	ProjectIcon,
 } from "../../../asset/icons/icons";
-import "./project-card.css"
+import "./project-card.css";
 import PropTypes from "prop-types";
 
 function ProjectCard({
@@ -26,46 +26,52 @@ function ProjectCard({
 	return (
 		<div className="w-96 h-96 rounded-3xl bg-gray-med p-8 text-white drop-shadow-lg flex flex-col gap-4 hover:scale-110 mt-8 transition-all ease-in">
 			<div className="text-orange flex justify-center items-center">
-				<ProjectCardImg/>
+				<ProjectCardImg />
 			</div>
 			<div className="flex flex-col gap-6">
 				<p className="text-2xl">{title}</p>
-				<p className=" text-white text-base line-clamp-4">
-					{description}
-				</p>
+				<p className=" text-white text-base line-clamp-4">{description}</p>
 
 				<div className="flex gap-4 justify-between items-center relative">
 					<p className="text-base">
 						Status: <span className={statusColor(status)}>{status}</span>
 					</p>
-                    <div className="flex flex-row flex-wrap gap-2">
-						<a className="project-card-link" data-tooltip="GitHub" href={gitUrl}>
+					<div className="flex flex-row flex-wrap gap-2">
+						<a
+							className="project-card-link"
+							data-tooltip="GitHub"
+							href={gitUrl}
+						>
 							<GithubIcon />
 						</a>
-						<a className="project-card-link" data-tooltip="Discord" href={discordUrl}>
+						<a
+							className="project-card-link"
+							data-tooltip="Discord"
+							href={discordUrl}
+						>
 							<DiscordIcon />
 						</a>
 						<a
-                        className="project-card-link" 
-                        data-tooltip="Project"
-                        href={projectUrl}>
+							className="project-card-link"
+							data-tooltip="Project"
+							href={projectUrl}
+						>
 							<ProjectIcon />
 						</a>
-                    </div>
+					</div>
 				</div>
 			</div>
 		</div>
 	);
 }
 
-ProjectCard.propTypes={
-	title:PropTypes.string.isRequired,
-	description:PropTypes.string.isRequired,
-	status:PropTypes.string.isRequired,
-	gitUrl:PropTypes.string.isRequired,
-	discordUrl:PropTypes.string.isRequired,
-	projectUrl:PropTypes.string.isRequired,
-}
+ProjectCard.propTypes = {
+	title: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	status: PropTypes.string.isRequired,
+	gitUrl: PropTypes.string.isRequired,
+	discordUrl: PropTypes.string.isRequired,
+	projectUrl: PropTypes.string.isRequired,
+};
 
 export default ProjectCard;
-
--- client/src/pages/projects/Projects.jsx
+++ client/src/pages/projects/Projects.jsx
@@ -3,23 +3,23 @@ import ProjectCard from "../../components/projects/project-card/Project-Card";
 import { projectsMap } from "./fakeProjectsData";
 
 function Projects() {
-	return( 
+	return (
 		<div className="flex justify-center items-center gap-6 flex-wrap">
-		{projectsMap &&
-			projectsMap.map((ele, key) => (
-				<React.Fragment key={key}>
-					<ProjectCard
-						title={ele.title}
-						description={ele.description}
-						status={ele.status}
-						gitUrl={ele.gitUrl}
-						discordUrl={ele.discordUrl}
-						projectUrl={ele.projectUrl}
-					/>
-				</React.Fragment>
-			))}
-	</div>
-	)
+			{projectsMap &&
+				projectsMap.map((ele, key) => (
+					<React.Fragment key={key}>
+						<ProjectCard
+							title={ele.title}
+							description={ele.description}
+							status={ele.status}
+							gitUrl={ele.gitUrl}
+							discordUrl={ele.discordUrl}
+							projectUrl={ele.projectUrl}
+						/>
+					</React.Fragment>
+				))}
+		</div>
+	);
 }
 
 export default Projects;
