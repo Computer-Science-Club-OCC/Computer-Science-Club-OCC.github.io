deb68930a13fc1d214d718a6cd178f14014f79fd
techlateef <techlateef@gmail.com>
2023-05-03 12:31:29 +0100

    added cors to the backend

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,18 @@ const app = express()
 // const passport = require("passport")
 // const magicLogin = require("passport-magic-login")
 const path = require("path")
+const cors = require('cors')
 require("dotenv/config")
+
+
+
+const corsOptions = {
+    origin:'http://localhost:3000',
+    methods:['GET', 'PUT', 'POST', 'PATCH','DELETE'],
+    optionsSuccessStatus:200,
+    allowedHeaders:['Content-Type', 'Authorization'],
+    credentials:true
+}
 
 // Import models
 // Notes: remove model imports won't add schemas to cs_club database on Linux
@@ -13,6 +24,7 @@ require("./models/projects/projects-model")
 require("./models/organizations/orgs-model")
 
 // Parsing
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static(path.join(path.resolve("./"), "public")))
